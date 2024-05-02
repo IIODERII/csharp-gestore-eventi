fa8dfcf2c611b8a56fd172f52a79e580224c9fb4
IIODERII <oderrisi@gmail.com>
2024-05-02 10:13:49 +0200

    Milestone 1 completed

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Evento
+    {
+        public string titolo;
+        public string Titolo
+        {
+            get
+            {
+                return titolo;
+            }
+            set
+            {
+                if(value != "")
+                {
+                    titolo = value;
+                }
+                else
+                {
+                    throw new Exception("Attenzione! Il titolo non può essere vuoto.");
+                }
+            }
+        }
+        public DateTime data;
+        public DateTime Data {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                if(value < DateTime.Now)
+                {
+                    throw new Exception("Attenzione! La data non può essere già passata.");
+                }
+                else
+                {
+                    data = value;
+                }
+            }
+        }
+        public int capienza;
+        public int Capienza
+        {
+            get
+            {
+                return capienza;
+            }
+            private set
+            {
+                if(value < 0)
+                {
+                    throw new Exception("Attenzione! La capienza massima dell'evento deve essere positiva.");
+                }
+                else
+                {
+                    capienza = value;
+                }
+            }
+        }
+        public int PostiPrenotati { get; private set; }
+
+        public Evento(string titolo, DateTime data, int capienza)
+        {
+            Titolo = titolo;
+            Data = data;
+            Capienza = capienza;
+            PostiPrenotati = 0;
+        }
+
+        public void PrenotaPosti(int posti) { 
+            if(posti < 0)
+                throw new Exception("Attenzione! Non puoi prenotare un numero negativo di posti.");
+
+            else if(Data >  DateTime.Now)
+                throw new Exception("Attenzione! L'evento è già passato.");
+
+            else if(PostiPrenotati + posti > Capienza)
+                throw new Exception($"Attenzione! Non ci sono {posti} posti disponibili.");
+
+            else
+                PostiPrenotati += posti; 
+        }
+
+        public void DisdiciPosti(int posti)
+        {
+            if(Data > DateTime.Now)
+                throw new Exception("Attenzione! L'evento è già passato.");
+
+            else if(PostiPrenotati - posti < 0)
+                throw new Exception("Attenzione! Il numero di posti prenotati è minore di quelli che si desidera disdire.");
+            
+            else
+                PostiPrenotati -= posti;
+        }
+
+        public override string ToString() => $"{Data.ToString("dd/MM/yyyy")} - {Titolo}";
+        
+    }
+}
